aabc4dada53317ad16552c061b7d6f4923fc8629
Mohammad Ahmed <ahmedghufran2010@gmail.com>
2026-08-13 15:55:19 +0500

    Python-Practice

--- Python Practice/main.py
+++ Python Practice/main.py
@@ -1,4 +1,5 @@
 print("Hello world!")
+
 # Arithmetic Operators
 a = 10
 b = 5
@@ -8,3 +9,13 @@ print("Multiplication:", a * b)
 print("Division:", a / b)
 print("Modulus:", a % b)
 print("Exponentiation:", a ** b)
+
+# Relational Operators
+a = 10
+b = 5
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater Than:", a > b)
+print("Less Than:", a < b)
+print("Greater Than or Equal:", a >= b)
+print("Less Than or Equal:", a <= b)
